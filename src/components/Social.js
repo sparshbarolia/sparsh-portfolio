328873bdbef2e7a99296d8a6ae9abcbc73d8b22a
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2023-12-10 00:36:56 +0530

    open social link in new tab

--- src/components/Social.js
+++ src/components/Social.js
@@ -11,19 +11,19 @@ export default function Social() {
             <ul className="social-ul">
                 <li className="social-li">
                     {/* resume pdf ko public folder me rkha h(ptani q) */}
-                    <a className="social-a" href='/resume.pdf'>
+                    <a className="social-a" target="_blank" href='/resume.pdf'>
                     {/* <SiGoogledocs /> */}
                     <FaFilePdf />
                     </a>
                     
                 </li>
                 <li className="social-li">
-                    <a className="social-a" href="https://www.linkedin.com/in/sparsh-976216221">
+                    <a className="social-a" target="_blank" href="https://www.linkedin.com/in/sparsh-976216221">
                     <FaLinkedin />
                     </a>
                 </li>
                 <li className="social-li">
-                    <a className="social-a" href="mailto:sparshbarolia@gmail.com">
+                    <a className="social-a" target="_blank" href="mailto:sparshbarolia@gmail.com">
                     <IoIosMail />
                     </a>
                     </li>
